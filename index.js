6a336bf3beefba5bc7261caba6e1e7db022e8d88
Biko23 <bikojeremy@gmail.com>
2020-07-28 10:55:30 +0300

    We have added the pages we require to the views folder for the start. These will be written using PUG so we need to download the pug engine and also set the view engine which we have done on line 6

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require ('express');
 
 const app = express();
 
-console.log('We have lift off....');
 
+app.set('view engine', 'pug');
+
+console.log('We have lift off....');
+app.get('/', (req, res) =>{
+    res.render('home');
+})
 app.listen(3000, (req, res) =>{
     console.log('Listening on port 3000....')
 })
